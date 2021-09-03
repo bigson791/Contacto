b6d98229c4d4c558698f87ddd4c8c8a9db6a9eb8
Elfego <bigson791@gmail.com>
2021-09-02 20:31:33 -0600

    creating the remote repository of the project

---
+++ contacto.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    <main>
+        <div class="container py-3">
+            <header class="mb-4 border-bottom">
+                <h1 class="fs-4">Contacto</h1>
+            </header>
+
+        </div>
+    </main>
+</body>
+</html>
